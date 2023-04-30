93b7c3e03bee8edc8cbaa6fa34082ade4ae8a9f8
MeshMeshNader <meshmeshnader555@gmail.com>
2023-04-30 14:32:17 +0300

    adding helloiti class

---
+++ HelloWorld/src/helloworld/HelloITI.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author meshm
+ */
+public class HelloITI {
+    public static void printHelloITI(){
+        System.out.println("Hello ITI");
+    }
+}
